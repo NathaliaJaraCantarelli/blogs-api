e196a5a0f5d3ee8b8baff99ab34273ae84ade1bc
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-03-08 02:25:19 -0300

    FEAT: Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        postId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+    },
+    {
+        timestamps: false,
+        underscored: true,
+        tableName: 'posts_categories',
+    });
+    PostCategory.associate = (models) => {
+        models.Category.belongsToMany(models.BlogPost, {
+            foreignKey: 'categoryId',
+            through: PostCategory,
+            otherKey: 'postId'
+        });
+        models.BlogPost.belongsToMany(models.Category, {
+            foreignKey: 'postId',
+            through: PostCategory,
+            otherKey: 'categoryId'
+        });
+    };
+    return PostCategory;
+};
